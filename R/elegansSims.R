1812d2ec6884dfc44db0917652d0dc46f0a89712
Joshua Bloom <joshsbloom@gmail.com>
2023-10-31 16:21:57 -0700

    switch out backend for AlphaSimR

--- R/elegansSims.R
+++ R/elegansSims.R
@@ -1,7 +1,8 @@
 library(xQTLStats)
-library(Meiosis)
+#library(Meiosis)
 library(vcfR)
 library(qs)
+library(AlphaSimR)
 #some more complex simulations here 
 
 #optional for doing simulations 
@@ -22,7 +23,7 @@ getGmapPositions=function(vcf.cross, gmap, uchr) {
    #where to put the variant sites, impute onto gmap
     imputed.positions=mapply( 
            function(x, y){
-                approxfun(y$ppos, y$map, rule=2)(x)
+                approxfun(y$ppos, y$map, rule=2,na.rm=T)(x)
             },
             x=p.by.chr, y=gmap,
             SIMPLIFY=F)
@@ -43,6 +44,7 @@ gmapRef=split(gmapRef, gmapRef$chrom)
 gmap=gmapRef
 sapply(gmap, function(x) max(x$map))
 gmap$X$map=gmap$X$map*(51/max(gmap$X$map))
+sapply(gmap, function(x) max(x$map))
 
 #data(crosses.to.parents)
 #genetic maps 
@@ -52,8 +54,8 @@ gmap$X$map=gmap$X$map*(51/max(gmap$X$map))
 #ref.vcf=system.file('reference', 'parents_w_svar_sorted.vcf.gz', package='xQTLStats')
 
 #pretty intensive memory usage here 
-big.vcf=vcfR::read.vcfR('/data0/elegans/xQTLSims/WI.20220216.impute.isotype.vcf.gz')
-qsave(big.vcf, file='/data0/elegans/xQTLSims/WI.20220216.vcf.qs')
+#big.vcf=vcfR::read.vcfR('/data0/elegans/xQTLSims/WI.20220216.impute.isotype.vcf.gz')
+#qsave(big.vcf, file='/data0/elegans/xQTLSims/WI.20220216.vcf.qs')
 
 vcf=qread('/data0/elegans/xQTLSims/WI.20220216.vcf.qs')
 #just get biallelic variants for now 
@@ -74,14 +76,11 @@ gt.sub=gt.sub[!( is.na(gt.sub[,1]) | is.na(gt.sub[,2])),]
 gt.sub=gt.sub[ !(gt.sub[,1]=='0|1' | gt.sub[,1]=='1|0' | 
                 gt.sub[,2]=='0|1' | gt.sub[,2]=='1|0' ),]
 #remove more complex structural stuff or mitochondrial variants for now
-gt.sub=gt.sub[!grepl('simple|deletion|complex|duplication|chrM', rownames(gt.sub)),]
+gt.sub=gt.sub[!grepl('simple|deletion|complex|duplication|chrM|MtDNA', rownames(gt.sub)),]
 #subset vcf ile
 vcf.cross=vcf[match(rownames(gt.sub), rownames(gt)), samples=c(p1.name,p2.name)]
 #generate sno ID
 vcf.cross=vcfR::addID(vcf.cross)
-
-
-
 
 #assuming input vcf with haploid parents and haploid specific variant calls 
 # [1] "273614xa" "BYa"      "CBS2888a" "CLIB219x" "CLIB413a" "I14a"    
@@ -95,7 +94,7 @@ vcf.cross=vcfR::addID(vcf.cross)
 #cross='A'
 
 #get the two parent names 
-#p1.name=crosses.to.parents[[cross]][1]
+#p1.name=crosses.to.parents[[cross]][1]gg
 #p2.name=crosses.to.parents[[cross]][2]
 
 #extract parental genotypes at variant sites between those parents from a reference vcf
@@ -113,6 +112,171 @@ uchr=c(as.character(as.roman(1:5)), 'X') #paste0('chr', as.roman(1:16))
 
 
 imputed.positions=jitterGmapVector(getGmapPositions(vcf.cross, gmap, uchr))
+
+#genetic map positions must be in Morgans
+genMap=data.frame(markerName=paste0(getCHROM(vcf.cross),'_',getPOS(vcf.cross)), chromosome=getCHROM(vcf.cross), position=unlist(imputed.positions)/100)
+
+teg.GT=t(eg.GT)
+#recode
+teg.GT[teg.GT==0]=-1
+colnames(teg.GT)=paste0(getCHROM(vcf.cross),'_',getPOS(vcf.cross))
+ped=data.frame(id=c('a', 'b'), mother=c(0,0), father=c(0,0))
+founderPop=importInbredGeno(geno=teg.GT, genMap=genMap)
+
+starting.sample.size=10
+
+#if mate or self, estimated brood size 
+brood.size.selfing=20
+brood.size.mating=brood.size.selfing*2
+
+#fraction of hermaphrodites that self
+selfing.rate=.2
+#
+
+max.per.gen=2e4
+
+max.gen=12
+
+
+SP=SimParam$new(founderPop)
+pop=newPop(founderPop, simParam=SP)
+crossPlan=matrix(c(1,2),nrow=1, ncol=2)
+
+#F1 ----------------------------------------------------------------------------------------------------------    
+nProgenyF1.mated=starting.sample.size*(1-selfing.rate)*brood.size.mating
+nProgenyF1.selfed=starting.sample.size*(selfing.rate)*brood.size.selfing
+FN=makeCross(pop, crossPlan, nProgeny=nProgenyF1.mated,simParam=SP)
+#t2=pullMarkerGeno(F1, colnames(teg.GT))
+#t2=pullMarkerGeno(F2.selfed, colnames(teg.GT))
+herm.index=sort(sample.int( nInd(FN), size=.5* nInd(FN)))
+male.index=which(!((1: nInd(FN)) %in% herm.index))
+
+ if(selfing.rate>0)  {
+        F2.selfed=makeCross(pop, matrix(c(1,1),nrow=1,ncol=2), nProgeny=nProgenyF1.selfed, simParam=SP)
+        herm.index=c(herm.index,nInd(FN)+seq(1,nInd(F2.selfed))) #length(F2.selfed)))
+        FN=c(FN, F2.selfed)
+    }
+#--------------------------------------------------------------------------------------------------------------
+
+    mated.herm=sample(herm.index, size=length(male.index))
+    mating.matrix=cbind(male.index,mated.herm,0)
+    mating.matrix=do.call(rbind, replicate(brood.size.mating, mating.matrix, simplify=F))
+
+    unmated.herm=herm.index[!(herm.index %in% mated.herm)]
+    self.cnt= round(length(herm.index)*selfing.rate)
+
+    if(self.cnt>0) {
+       selfings=sample(unmated.herm, self.cnt)
+       selfing.matrix=cbind(selfings, selfings,1)
+       selfing.matrix=do.call(rbind, replicate(brood.size.selfing, selfing.matrix, simplify=F))
+       mating.matrix=rbind(mating.matrix, selfing.matrix)
+    }
+    if(nrow(mating.matrix)>max.per.gen){
+        mating.matrix=mating.matrix[sample(1:nrow(mating.matrix), max.per.gen),]
+        mating.matrix=mating.matrix[order(mating.matrix[,3]),]
+    }
+   #----------------------------------------------------------------------------------------------------------
+
+
+   current.gen=2
+   while(current.gen<=max.gen) {
+       print(current.gen) 
+       #at least loop here 
+       mated.index=which(mating.matrix[,3]==0)
+       selfed.index=which(mating.matrix[,3]==1)
+       print(paste('mated', length(mated.index)))
+       print(paste('selfed', length(selfed.index)))
+
+       FN=makeCross(FN, mating.matrix[,-3], nProgeny=1,simParam=SP)
+
+       #sanity check selfing 
+       #FN3=makeCross(FN2[1], matrix(c(1,1), nrow=1), nProgeny=1)
+       #t2=pullMarkerGeno(FN3[1], colnames(teg.GT))
+       #FN3=makeCross(FN3[1], matrix(c(1,1), nrow=1), nProgeny=1)
+       #t2=pullMarkerGeno(FN3[1], colnames(teg.GT))
+       #plot(t2[1,])
+
+       #FN=apply(mating.matrix, 1, function(x) { Meiosis::cross_geno(father=FN[[x[1]]], mother=FN[[x[2]]],  positions = imputed.positions, xoparam=xoparam) })
+       
+       #half of the progeny from matings will be hermaphrodites
+       herm.index=sort(sample(mated.index,size=length(mated.index)/2))
+       #the rest of the mated progeny will be males 
+       male.index=which(!((mated.index %in% herm.index)))
+       #all of the selfed progeny will by hermaphrodites
+       if(length(selfed.index)>0) {
+            herm.index=c(herm.index,selfed.index)
+        }
+       print(paste('total herm', length(herm.index)))
+       print(paste('total male', length(male.index)))
+       
+       #for all the males, sample from hermaphrodites for them to mate with
+       mated.herm=sample(herm.index, size=min(length(herm.index), length(male.index)))
+       mating.matrix=cbind(male.index,mated.herm,0)
+
+       #generate cross progeny for matings, with brood size dependent on expected brood size if mating 
+       mating.matrix=do.call(rbind, replicate(brood.size.mating, mating.matrix, simplify=F))
+      
+       #note which hermaphrodites aren't mated 
+       unmated.herm=herm.index[!(herm.index %in% mated.herm)]
+
+       #how many do we expected to self given the total number of hermaphrodites and the selfing rate 
+       self.cnt= round(length(herm.index)*selfing.rate)
+       #if we expect any selfers then ... 
+       if(self.cnt>0) {
+           #figure out which ones self 
+           selfings=sample(unmated.herm, min(length(unmated.herm),self.cnt))
+           #make a selfing matrix
+           selfing.matrix=cbind(selfings, selfings,1)
+           #make brood size reflect brood size per selfing
+           selfing.matrix=do.call(rbind, replicate(brood.size.selfing, selfing.matrix, simplify=F))
+           mating.matrix=rbind(mating.matrix, selfing.matrix)
+        }
+       #total worms at this point 
+       print(nrow(mating.matrix))
+
+       #bottleneck
+       if(nrow(mating.matrix)>max.per.gen){
+            mating.matrix=mating.matrix[sample(1:nrow(mating.matrix), max.per.gen),]
+            mating.matrix=mating.matrix[order(mating.matrix[,3]),]
+       }
+       current.gen=current.gen+1
+   }
+
+
+t2=pullMarkerGeno(FN[1:1000], colnames(teg.GT))
+
+rcnt=colSums(t2)
+plot(rcnt/(nrow(t2)*2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #how many chromosomes
@@ -132,21 +296,33 @@ brood.size.selfing=20
 brood.size.mating=brood.size.selfing*2
 
 #fraction of hermaphrodites that self
-selfing.rate=.1
+selfing.rate=.2
 
 max.per.gen=1e4
 
 max.gen=12
 
+#simcross
+#p1=create_parent(L=55.56, allele=0)
+#p2=create_parent(L=55.56, allele=1)
+#p3=create_parent(L=55.56, allele=2)
+#p4=create_parent(L=55.56, allele=3)
+#p3=create_parent(L=55.56, allele=2)
+#F1.1=cross(p1,p2,m=0, obligate_chiasma=T)
+#F1.2=cross(p3,p4,m=0, obligate_chiasma=T)
+#F2=cross(F1.1,F1.2)
+#F2.pop=replicate(1000,cross(F1,F1))
+
+
 
  #parental haplotypes
-    p1.ind=split(as.numeric(eg.GT[,1]), vcfR::getCHROM(vcf.cross))
+    p1.ind=split(as.numeric(eg.GT[,1])+1, vcfR::getCHROM(vcf.cross))
     p1.ind=p1.ind[uchr]
-    p2.ind=split(as.numeric(eg.GT[,2]), vcfR::getCHROM(vcf.cross))
+    p2.ind=split(as.numeric(eg.GT[,2])+1, vcfR::getCHROM(vcf.cross))
     p2.ind=p2.ind[uchr]
 
     #creation of mated F1
-    ind=list(p1.ind, p2.ind)
+    ind=list(p1.ind[1], p2.ind[1])
     
     #creation of non.mated F1   
     ind2=list(p1.ind, p1.ind)
